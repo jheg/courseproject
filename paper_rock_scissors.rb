1141d86367ded9d3577aa71fccb1cf86394e7de9
jheg <jason@jheg.co.uk>
2014-08-13 19:16:21 +0100

    tidied indentation

--- paper_rock_scissors.rb
+++ paper_rock_scissors.rb
@@ -4,9 +4,7 @@ def say(msg)
   puts "--- #{msg} ---"
 end
 
-
 objects = %w(Paper Rock Scissors)
-
 again = 'y'
 
 while again == 'y'
@@ -15,43 +13,41 @@ while again == 'y'
   ans = gets.chomp
   ans.upcase!
 
-  while ans != 'P' && ans != 'R' && ans != 'S'
-    puts "Choose One: (P/R/S)"
-    ans = gets.chomp
-  end
+    while ans != 'P' && ans != 'R' && ans != 'S'
+      puts "Choose One: (P/R/S)"
+      ans = gets.chomp
+    end
 
-if ans == "P"
-  users_choice = objects[0]
-elsif ans == "R"
-  users_choice = objects[1]
-else
-  users_choice = objects[2]
-end
-binding.pry
+    if ans == "P"
+      users_choice = objects[0]
+    elsif ans == "R"
+      users_choice = objects[1]
+    else
+      users_choice = objects[2]
+    end
     
-
 
   comp = objects.sample
   say("You chose #{users_choice}")
   say("Computer chose #{comp}")
 
 
-  if users_choice == comp  
-    puts "You picked #{users_choice} and computer picked #{comp}"
-    puts "its a draw!"
-  elsif
-    users_choice == "Paper" && comp == "Rock"
-    puts "#{users_choice} covers #{comp}"
-    puts "You win!"
-  elsif users_choice == "Paper" && comp == "Scissors"
-    puts "#{comp} cuts #{users_choice}"
-    puts "You lose!"
-  elsif users_choice == "Rock" && comp == "Paper"
-    puts "#{comp} covers #{users_choice}"
-    puts "You lose!"
-  else
-    puts "something else"
-  end
+    if users_choice == comp  
+      puts "You picked #{users_choice} and computer picked #{comp}"
+      puts "its a draw!"
+    elsif
+      users_choice == "Paper" && comp == "Rock"
+      puts "#{users_choice} covers #{comp}"
+      puts "You win!"
+    elsif users_choice == "Paper" && comp == "Scissors"
+      puts "#{comp} cuts #{users_choice}"
+      puts "You lose!"
+    elsif users_choice == "Rock" && comp == "Paper"
+      puts "#{comp} covers #{users_choice}"
+      puts "You lose!"
+    else
+      puts "something else"
+    end
 
 puts "Play again? (Y/N)"
 again = gets.chomp
