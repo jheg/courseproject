ab24279434090963fecc40cecd644cea7d8e7e08
jheg <jason@jheg.co.uk>
2014-08-13 11:35:07 +0100

    upped scope example

---
+++ scope.rb
@@ -0,0 +1,15 @@
+num1 = 5
+num2 = 1
+num3 = num1 + num2
+
+if num3 < 5
+  result = 'num3 is less than 5'
+  puts result
+else
+  result = 'num3 is more than 5'
+  puts result
+end
+
+puts result
+
+
