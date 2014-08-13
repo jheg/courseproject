6c1788a6aa10a5de7b82a65fe56c5fcd2655c308
jheg <jason@jheg.co.uk>
2014-08-13 16:08:24 +0100

    PRS game started

---
+++ paper_rock_scissors.rb
@@ -0,0 +1,42 @@
+require 'pry'
+
+def say(msg)
+  puts "--- #{msg} ---"
+end
+
+
+objects = %w(Paper Rock Scissors)
+p = 0
+r = 1
+s = 2
+
+
+puts "Choose One: (P/R/S)"
+ans = gets.chomp
+while ans != 'p' && ans != 'r' && ans != 's'
+  puts "Choose One: (P/R/S)"
+  ans = gets.chomp
+end
+users_choice = objects[ans.to_i]
+comp = objects.sample
+say("You chose #{users_choice}")
+say("Computer chose #{comp}")
+
+
+if users_choice == comp  
+  puts "You picked #{users_choice} and computer picked #{comp}"
+  puts "its a draw!"
+elsif
+  users_choice == "Paper" && comp == "Rock"
+  puts "#{users_choice} covers #{comp}"
+  puts "You win!"
+elsif users_choice == "Paper" && comp == "Scissors"
+  puts "#{comp} cuts #{users_choice}"
+  puts "You lose!"
+else
+  puts "something else"
+end
+
+
+
+
