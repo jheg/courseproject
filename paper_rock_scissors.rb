63f1d5eb71c214b07c727e6498e435a7fed1564f
jheg <jason@jheg.co.uk>
2014-08-13 16:14:08 +0100

    added restart option

--- paper_rock_scissors.rb
+++ paper_rock_scissors.rb
@@ -10,32 +10,44 @@ p = 0
 r = 1
 s = 2
 
+again = 'y'
 
-puts "Choose One: (P/R/S)"
-ans = gets.chomp
-while ans != 'p' && ans != 'r' && ans != 's'
+while again == 'y'
+
   puts "Choose One: (P/R/S)"
   ans = gets.chomp
-end
-users_choice = objects[ans.to_i]
-comp = objects.sample
-say("You chose #{users_choice}")
-say("Computer chose #{comp}")
+  while ans != 'p' && ans != 'r' && ans != 's'
+    puts "Choose One: (P/R/S)"
+    ans = gets.chomp
+  end
+  users_choice = objects[ans.to_i]
+  comp = objects.sample
+  say("You chose #{users_choice}")
+  say("Computer chose #{comp}")
 
 
-if users_choice == comp  
-  puts "You picked #{users_choice} and computer picked #{comp}"
-  puts "its a draw!"
-elsif
-  users_choice == "Paper" && comp == "Rock"
-  puts "#{users_choice} covers #{comp}"
-  puts "You win!"
-elsif users_choice == "Paper" && comp == "Scissors"
-  puts "#{comp} cuts #{users_choice}"
-  puts "You lose!"
-else
-  puts "something else"
+  if users_choice == comp  
+    puts "You picked #{users_choice} and computer picked #{comp}"
+    puts "its a draw!"
+  elsif
+    users_choice == "Paper" && comp == "Rock"
+    puts "#{users_choice} covers #{comp}"
+    puts "You win!"
+  elsif users_choice == "Paper" && comp == "Scissors"
+    puts "#{comp} cuts #{users_choice}"
+    puts "You lose!"
+  else
+    puts "something else"
+  end
+  
+puts "Play again? (Y/N)"
+again = gets.chomp
+
 end
+
+
+
+
 
 
 
