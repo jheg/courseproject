31c20a9f81c9a212ce5e3efb4f7ed42365124a0b
jheg <jason@jheg.co.uk>
2014-08-25 22:31:50 +0100

    tidy up

--- ttt.rb
+++ ttt.rb
@@ -5,10 +5,9 @@
 # Player/Computer has a turn if square available
 # check for winner
 
-# play again?
-
 require 'pry'
 
+# play again?
 play_again = 'y'
 
 while play_again == 'y'
@@ -17,50 +16,51 @@ while play_again == 'y'
 board_squares = {1 => " ",2 => " ",3 => " ",4 => " ",5 => " ",6 => " ",7 => " ",8 => " ",9 => " "}
 
 # Available squares
-def available_squares(h)
-  h.select {|_,v| v == " "}.keys
+def available_squares(squares)
+  squares.select {|_,v| v == " "}.keys
 end
 
 # players stored in array so player can be chosen at random
 players = ["player1", "player2"]
 
 # draw the board 
-def draw_board(s)
+def draw_board(squares)
   system 'clear'
-  puts "#{s[1]}|#{s[2]}|#{s[3]}"
+  puts "#{squares[1]}|#{squares[2]}|#{squares[3]}"
   puts "-----"
-  puts "#{s[4]}|#{s[5]}|#{s[6]}"
+  puts "#{squares[4]}|#{squares[5]}|#{squares[6]}"
   puts "-----"
-  puts "#{s[7]}|#{s[8]}|#{s[9]}"
+  puts "#{squares[7]}|#{squares[8]}|#{squares[9]}"
 end
 
+# player/computer picks square methods
+def player1(squares) 
+    if available_squares(squares).any?
+      puts "Choose an available square from #{available_squares(squares)}"
+      i = gets.chomp.to_i    
+        if available_squares(squares).include?(i)
+          squares[i] = "x"
+        else
+          player1(squares)
+        end     
+      draw_board(squares)
+    end
+end
+def player2(squares)
+    puts "Computer chooses a square"
+    sleep 0.5
+    i = available_squares(squares).sample
+    squares[i] = "o"
+    draw_board(squares)
+end
+
+# setting the who goes first variable
+goes_first = players.sample
 
 # show players the empty board
 draw_board(board_squares)
 
-# player/computer picks square methods
-def player1(g) 
-    if !available_squares(g).empty?
-      puts "Choose an available square from #{available_squares(g)}"
-      i = gets.chomp.to_i    
-        if available_squares(g).include?(i)
-          g[i] = "x"
-        else
-          player1(g)
-        end     
-      draw_board(g)
-    end
-end
-def player2(g)
-    puts "Computer chooses a square"
-    sleep 1
-    i = available_squares(g).sample
-    g[i] = "o"
-    draw_board(g)
-end
-
-goes_first = players.sample
-
+# Conditional that checks which loop to execute: player 1 or player 2
 if goes_first == 'player1'
   begin
     player1(board_squares)
@@ -73,34 +73,20 @@ elsif goes_first == 'player2'
   end until available_squares(board_squares).empty?
 end
 
+sleep 0.5
 puts "GAME OVER"
+
+
+# play again?
+sleep 0.5
 puts "Play again? (y/n)"
 play_again = gets.chomp
 
 end
 
-# Run conditional to decide which method to invoke - player or computer
-# def goes_first(a)
-#   first = a.sample
-#   return first
-# end
-
-# no_one = goes_first(players)
-
-# if no_one == "player"
-#   puts "You go first!"
-#   player(board_squares)
-# elsif no_one == "computer"
-#   puts "Wait your turn..."
-#   computer(board_squares)
-# end
 
 
-# play again?
-# until play_again == "y" || play_again == "n"
-#   puts "Play again? (type y or n)"
-#   play_again = gets.chomp
-# end
+
 
 
    
